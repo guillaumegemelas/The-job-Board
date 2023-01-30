801b244974906333605c7bd4c481dacc622100b7
Guillaume Gemelas <GuiGui@MacBook-Pro.local>
2023-01-30 17:33:28 +0100

    verif exo2

--- src/App.js
+++ src/App.js
@@ -1,93 +1,12 @@
 import "./App.css";
+import Job from "./components/Job";
 import Jobs from "./components/Jobs";
 
 function App() {
   return (
     <div className="bloc">
       <header className="title">The Job Board</header>
-      <div className="container">
-        <div class="item">
-          <Jobs
-            className="Full Time Sales"
-            title="Associate-Sydney boutique"
-            contracType="CDI"
-            country="Australie"
-            city="Sydney"
-          ></Jobs>
-        </div>
-        <div class="item">
-          <Jobs
-            className="Agent de sécurité"
-            title="Pantin"
-            contracType="CDI"
-            country="France"
-            city="Pantin"
-          ></Jobs>
-        </div>
-        <div class="item">
-          <Jobs
-            className="Responsable d'Atelier (H/F)"
-            title=""
-            contracType="CDD"
-            country="France"
-            city="Paris"
-          ></Jobs>
-        </div>
-        <div class="item">
-          <Jobs
-            className="Chef de Projets"
-            title=""
-            contracType="CDD"
-            country="France"
-            city="Paris"
-          ></Jobs>
-        </div>
-        <div class="item">
-          <Jobs
-            className="Développeur React.js"
-            title=""
-            contracType="CDI"
-            country="France"
-            city="Paris"
-          ></Jobs>
-        </div>
-        <div class="item">
-          <Jobs
-            className="Sales Associate"
-            title="Stockholm"
-            contracType="CDI"
-            country="Suède"
-            city="Stockholm"
-          ></Jobs>
-        </div>
-        <div class="item">
-          <Jobs
-            className="Vendeur/se - Crans"
-            title="Montana"
-            contracType="CDI"
-            country="Suisse"
-            city="Crans-Montana"
-          ></Jobs>
-        </div>
-        <div class="item">
-          <Jobs
-            className="CRM & Data Quality"
-            title="Analyst"
-            contracType="CDI"
-            country="USA"
-            city="New-York"
-          ></Jobs>
-        </div>
-        <div class="item">
-          <Jobs
-            className="Infirmier (H/F)"
-            title=""
-            contracType="CDI"
-            country="France"
-            city="Pantin"
-          ></Jobs>
-        </div>
-      </div>
+      <Jobs />
       <footer className="end">
         <p>Made with React at Le Reacteur by Guillaume</p>
       </footer>
--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,15 +1,89 @@
+import Job from "./Job";
+
 const Jobs = (props) => {
   console.log(props);
   return (
-    <div className="boxs">
-      <div className="headBox">
-        <p>{props.className}</p>
-        <p>{props.title}</p>
+    <div className="container">
+      <div className="job">
+        <Job
+          className="Full Time Sales"
+          title="Associate-Sydney boutique"
+          contracType="CDI"
+          country="Australie"
+          city="Sydney"
+        />
       </div>
-      <div className="endBox">
-        <p>{props.contracType}</p>
-        <p>{props.country}</p>
-        <p>{props.city}</p>
+      <div className="job">
+        <Job
+          className="Agent de sécurité"
+          title="Pantin"
+          contracType="CDI"
+          country="France"
+          city="Pantin"
+        />
+      </div>
+      <div className="job">
+        <Job
+          className="Responsable d'Atelier (H/F)"
+          title=""
+          contracType="CDD"
+          country="France"
+          city="Paris"
+        />
+      </div>
+      <div className="job">
+        <Job
+          className="Chef de Projets"
+          title=""
+          contracType="CDD"
+          country="France"
+          city="Paris"
+        />
+      </div>
+      <div className="job">
+        <Job
+          className="Développeur React.js"
+          title=""
+          contracType="CDI"
+          country="France"
+          city="Paris"
+        />
+      </div>
+      <div className="job">
+        <Job
+          className="Sales Associate"
+          title="Stockholm"
+          contracType="CDI"
+          country="Suède"
+          city="Stockholm"
+        />
+      </div>
+      <div className="job">
+        <Job
+          className="Vendeur/se - Crans"
+          title="Montana"
+          contracType="CDI"
+          country="Suisse"
+          city="Crans-Montana"
+        />
+      </div>
+      <div className="job">
+        <Job
+          className="CRM & Data Quality"
+          title="Analyst"
+          contracType="CDI"
+          country="USA"
+          city="New-York"
+        />
+      </div>
+      <div className="job">
+        <Job
+          className="Infirmier (H/F)"
+          title=""
+          contracType="CDI"
+          country="France"
+          city="Pantin"
+        />
       </div>
     </div>
   );
