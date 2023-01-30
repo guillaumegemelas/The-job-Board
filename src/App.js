02e2ec31c5cbeda4963e670840ba3934ecc6d5e0
Guillaume Gemelas <GuiGui@MacBook-Pro.local>
2023-01-30 15:47:02 +0100

    trame ok

--- src/App.js
+++ src/App.js
@@ -2,8 +2,22 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="bloc">
+      <header className="title"></header>
+      <div className="container">
+        <div class="item"></div>
+        <div class="item"></div>
+        <div class="item"></div>
+        <div class="item"></div>
+        <div class="item"></div>
+        <div class="item"></div>
+        <div class="item"></div>
+        <div class="item"></div>
+        <div class="item"></div>
+      </div>
+      <footer className="end">
+        <p>Made with React at Le Reacteur by Guillaume</p>
+      </footer>
     </div>
   );
 }
